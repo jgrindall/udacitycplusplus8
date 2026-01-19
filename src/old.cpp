f8ae73bbc9d088a2a50e0f2c573635a1317f5c8b
johngrindall <john@2simple.com>
2026-01-19 18:45:11 +0000

    old stuff from prev project

---
+++ src/old.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <future>
+#include <mutex>
+#include <algorithm>
+
+class Vehicle
+{
+public:
+    Vehicle(int id) : _id(id) {}
+    int getID() { return _id; }
+
+private:
+    int _id;
+};
+
+template<typename T>
+class Queue
+{
+public:
+    Queue() {}
+
+    T get()
+    {
+        // perform vector modification under the lock
+        std::unique_lock<std::mutex> uLock(_mutex);
+        _cond.wait(uLock, [this] {
+            return !_items.empty(); 
+        });
+
+        // remove last vector element from queue
+        T v = std::move(_items.back());
+        _items.pop_back();
+
+        return v; // will not be copied due to return value optimization (RVO) in C++
+    }
+
+    void add(T &&v)
+    {
+        // simulate some work
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        // perform vector modification under the lock
+        std::lock_guard<std::mutex> uLock(_mutex);
+
+        // add vector to queue
+        std::cout << "    #" << v.getID() << " will be added to the queue" << std::endl;
+        _items.push_back(std::move(v));
+        _cond.notify_one(); // notify client after pushing new Vehicle into vector
+    }
+
+private:
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::vector<T> _items; // list of all vehicles waiting to enter this intersection
+};
+
+int main_old()
+{
+    // create monitor object as a shared pointer to enable access by multiple threads
+    std::shared_ptr<Queue<Vehicle>> queue(new Queue<Vehicle>);
+
+    std::cout << "Spawning threads..." << std::endl;
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 10; ++i)
+    {
+        // create a new Vehicle instance and move it into the queue
+        Vehicle v(i);
+        futures.emplace_back(std::async(std::launch::async, &Queue<Vehicle>::add, queue, std::move(v)));
+    }
+
+    std::cout << "Collecting results..." << std::endl;
+    while (true)
+    {
+        // popBack wakes up when a new element is available in the queue
+        Vehicle v = queue->get();
+        std::cout << "   Vehicle #" << v.getID() << " has been removed from the queue" << std::endl;
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+
+    std::cout << "Finished!" << std::endl;
+
+    return 0;
+}
